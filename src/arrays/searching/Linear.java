1900a3087fefc58384cb71240df7593475d5db48
lucasNSF <nascimentomr10@gmail.com>
2024-01-17 16:54:26 -0300

    Linear Search Algorithm

---
+++ src/arrays/searching/Linear.java
@@ -0,0 +1,10 @@
+package arrays.searching;
+
+public class Linear {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+}
