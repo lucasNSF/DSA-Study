3caaae702cd20df7c8d336fc7a531ccfff49b2ee
lucasNSF <nascimentomr10@gmail.com>
2024-01-17 16:55:12 -0300

    Array Test Class

---
+++ src/arrays/searching/ArrayTest.java
@@ -0,0 +1,28 @@
+package arrays.searching;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//        Test Searching Algorithms
+
+//        System.out.println("Linear Search");
+//        System.out.println(Linear.search(arr, 6));
+
+//        System.out.println("Binary Search");
+//
+//        System.out.println("Iterative");
+//        System.out.println(Binary.iterativeSearch(arr, 8));
+//
+//        System.out.println("Recursive");
+//        System.out.println(Binary.recursiveSearch(arr, 8));
+
+//        System.out.println("Meta Binary Search");
+//        System.out.println(MetaBinary.search(arr, 5));
+
+        System.out.println("Ternary Search");
+
+        System.out.println("Iterative");
+        System.out.println(TernarySearch.iterativeSearch(arr, 5));
+    }
+}
